75effa4ec1d38f823e19fcfc697accb5eceda90d
KylePearce-SoftwareDeveloper <kylepearcesoftwaredeveloper@gmail.com>
2021-03-31 16:24:27 +0100

    #5 - Fifth test passed.

--- Test.Mma.Common/WindFormatterTests.cs
+++ Test.Mma.Common/WindFormatterTests.cs
@@ -36,5 +36,26 @@
             Assert.That(result, Is.EqualTo(expected));
         }
 
+        [TestCase(2, 90, 190, "VRB02G28KT")]
+        [TestCase(3, 100, 120, "09003G28KT")]
+        [TestCase(3, 100, 200, "VRB03G28KT")]
+        [TestCase(10, 200, 250, "09010G28KT")]
+        [TestCase(20, 200, 350, "09020KT200V350")]
+        public void Average_wind_direction_is_correct_extra(double? averageSpeed, double? minDirection, double? maxDirection, string expected)
+        {
+            var data = new WindData
+            {
+                AverageWindDirection = 90,
+                AverageWindSpeed = averageSpeed,
+                MaximumWindSpeed = 28,
+                MinimumWindDirection = minDirection,
+                MaximumWindDirection = maxDirection
+            };
+
+            var result = formatter.FormatWind(data);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
     }
 }
